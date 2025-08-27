7384ef52c5d458d26d4fe2394bb0848b77b3180a
Bonthu Sai Snehith <139102123+SaiSnehithBonthu@users.noreply.github.com>
2025-08-27 15:13:53 +0530

    Update page title and main title for Montessori AI Chat AssistantUpdate titles to reflect Montessori AI Chat Assistant brandingUpdate app.py

--- app.py
+++ app.py
@@ -9,14 +9,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
 # read all pdf files and return text
-
-
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -24,36 +20,26 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
-
 # split text into chunks
-
-
 def get_text_chunks(text):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=10000, chunk_overlap=1000)
     chunks = splitter.split_text(text)
     return chunks  # list of strings
-
 # get embeddings for each chunk
-
-
 def get_vector_store(chunks):
     embeddings = GoogleGenerativeAIEmbeddings(
         model="models/embedding-001")  # type: ignore
     vector_store = FAISS.from_texts(chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
-
-
 def get_conversational_chain():
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
-
     Answer:
     """
-
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                                    client=genai,
                                    temperature=0.3,
@@ -62,35 +48,24 @@ def get_conversational_chain():
                             input_variables=["context", "question"])
     chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
     return chain
-
-
 def clear_chat_history():
     st.session_state.messages = [
         {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
-
-
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(
         model="models/embedding-001")  # type: ignore
-
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True) 
     docs = new_db.similarity_search(user_question)
-
     chain = get_conversational_chain()
-
     response = chain(
         {"input_documents": docs, "question": user_question}, return_only_outputs=True, )
-
     print(response)
     return response
-
-
 def main():
     st.set_page_config(
-        page_title="Gemini PDF Chatbot",
+        page_title="Montessori AI Chat Assistant",
         page_icon="🤖"
     )
-
     # Sidebar for uploading PDF files
     with st.sidebar:
         st.title("Menu:")
@@ -102,28 +77,22 @@ def main():
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
                 st.success("Done")
-
     # Main content area for displaying chat messages
-    st.title("Chat with PDF files using Gemini🤖")
+    st.title("Chat with your Montessori PDFs 🤖")
     st.write("Welcome to the chat!")
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
     # Chat input
     # Placeholder for chat messages
-
     if "messages" not in st.session_state.keys():
         st.session_state.messages = [
             {"role": "assistant", "content": "upload some pdfs and ask me a question"}]
-
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.write(message["content"])
-
     if prompt := st.chat_input():
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.write(prompt)
-
     # Display chat messages and bot response
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
@@ -138,7 +107,5 @@ def main():
         if response is not None:
             message = {"role": "assistant", "content": full_response}
             st.session_state.messages.append(message)
-
-
 if __name__ == "__main__":
     main()
